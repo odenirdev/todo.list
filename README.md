b6054ec752a41c760c627bba5e6a7da0b027dd46
Odenir <odenirdev@gmail.com>
2022-03-30 11:34:50 -0300

    Add: README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# Todo.list
+
+> App de lista de afazeres
+
+Tem como objetivo, auxiliar na organização e produtividade, lembrando o usuário sobre seu afazeres.
+
+## Como rodar o projeto
+
+### Ambiente de desenvolvimento
+
+```bash
+yarn dev
+```
+
+## Referências
+
+Aplicação desenvolvido como desafio do curso Ignite da Rocketseat.
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,32 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState("");
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTask: Task = {
+      id: tasks.length + 1,
+      title: newTaskTitle,
+      isComplete: false,
+    };
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle("");
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map((task) => {
+      if (task.id !== id) return task;
+
+      return { ...task, isComplete: true };
+    });
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter((task) => task.id !== id);
+
+    setTasks(newTasks);
   }
 
   return (
